4fad86b7cccb33512bc9131b84cc8cfa10e39f40
balabambe <balabambe@gmail.com>
2021-11-29 16:47:36 +0800

    Ordering Exercise.

--- ConsoleApp2/ArrayOrdering.cs
+++ ConsoleApp2/ArrayOrdering.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public class ArrayOrdering
 {
@@ -9,11 +10,56 @@ public class ArrayOrdering
     }
     public static void Go()
 	{
-		int[] array = { 5, 6, 2, 99, 1, 108, 12 };
-		foreach (int item in array)
+        List<int> numbers = inputNumbers();
+
+        if (numbers.Count == 0)
         {
-            Console.WriteLine(item);
+            Console.WriteLine("沒有數字可以排序。");
+            return;
         }
-        Console.WriteLine(array.ToString());
+
+        Console.WriteLine("排序前：[{0}]", string.Join(", ", numbers));
+        int swapCount;
+        do
+        {
+            swapCount = 0;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (i == (numbers.Count - 1))
+                {
+                    break;
+                }
+                if (numbers[i] > numbers[i + 1])
+                {
+                    int swapNumber = numbers[i];
+                    numbers[i] = numbers[i + 1];
+                    numbers[i + 1] = swapNumber;
+                    swapCount++;
+                }
+            }
+        } while (swapCount != 0);
+        Console.WriteLine("排序後：[{0}]", string.Join(", ", numbers));
 	}
+
+    private static List<int> inputNumbers()
+    {
+        List<int> numbers = new List<int> { };
+
+        while (true)
+        {
+            try
+            {
+                Console.Write($"請輸入數字[{string.Join(", ", numbers)}]：");
+                string inputNumber = Console.ReadLine();
+                int value = Convert.ToInt32(inputNumber);
+                numbers.Add(value);
+            }
+            catch {
+                break;
+            }
+        }
+
+
+        return numbers;
+    }
 }
